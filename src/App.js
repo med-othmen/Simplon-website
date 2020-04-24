14436680cdb017aa985e615a4e39b30f2878c03f
Othmen mohamed ali <mohamedali.othmen.simplon@gmail.com>
2020-04-24 12:28:20 +0100

    first components: intro to team-page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import Introequipe from  './equipe/intro-equipe/index'  // importation de la composent intro-equipe 
 
 function App() {
   return (
     <div className="App">
-  
+
+
+  <Introequipe />
+
+
+
     </div>
-  );
+  )
 }
 
 export default App;
