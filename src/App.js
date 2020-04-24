eb6540e0049e2f17dcd67a1be949ba592c662f16
Othmen mohamed ali <mohamedali.othmen.simplon@gmail.com>
2020-04-24 16:49:14 +0100

    add fourth component : staff pictures

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Introequipe from  './equipe/intro-equipe/index'  // importation de la composent intro-equipe 
 import Pictures from  './equipe/pictures-viewer/index'  // importation de la composent pictures-viewer
 import LogoCode from './equipe/logo-InCodeWeTrust/index' // importation de la composent logo In Code We trust 
-import Team from './equipe/logo-InCodeWeTrust/index' // importation de la composent team section 
+import Team from './equipe/team/index' // importation de la composent team section 
 
 // importation des images localement
 import img1 from './equipe/pictures-viewer/slide-photos/image1.JPG' 
@@ -17,22 +17,30 @@ import img6 from './equipe/pictures-viewer/slide-photos/image6.JPG'
 // importer le logo de 'In Code we trust ' 
 import imgCode from './equipe/img/simplon-wetrust.png'
 
+// importer les images d'équipes 
+import directrice from './equipe/img/team-pictures/directrice.jpeg'
+import formateur from './equipe/img/team-pictures/formateur.jpg'
+import responsable from './equipe/img/team-pictures/responsable.jpg'
 
 // tableau des images pour l"utiliser dans la composent 'pictures-viewer'
 const pictSlides=[img1, img2, img3,img4,img5,img6 ]
 
+// tableau des images d'équipes pour l'utiliser dans la composent 'team'
+const teampictures=[
+ {name: "Mouna ayari", src: directrice},
+ {name: "Mohamed seif hamza", src: formateur},
+ {name: "Syrine Auguste Amy", src: responsable}
+]
 
 function App() {
+
   return (
     <div className="App">
 
-        <Introequipe />  
-
-        <Pictures pictSlides={pictSlides} />
-      
-      <LogoCode  imgCode={imgCode}/>
-
-     
+      <Introequipe />  
+      <Pictures pictSlides={pictSlides} />
+      <LogoCode  imgCode={imgCode}      />
+      <Team teampictures={teampictures} />
 
     </div>
   )
---
+++ src/equipe/team/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+function Team(props) {
+  return (
+    <div className="teamsection">
+     {props.teampictures.map(el =>
+
+            <div className="cards">
+    
+             <img className="card" src={el.src} alt="logo " />
+             <h1 className="name">  {el.name} </h1> 
+
+            </div>      
+              )
+          }
+
+  </div>
+  )
+}
+
+export default Team;
